f43c789516c2b9399ccedc8de6d78a6834df13f2
Naomi Dushay <ndushay@stanford.edu>
2012-04-17 22:58:15 -0700

    creeping up on running specs

--- lib/solrj_wrapper/settings.rb
+++ lib/solrj_wrapper/settings.rb
@@ -6,7 +6,7 @@ class Settings
   attr_reader :solr_url, :solrj_jar_dir, :solrj_queue_size, :solrj_num_threads
   
   def initialize(settings_group)
-    yml = YAML.load_file('config/settings.yml')[settings_group]
+    yml = YAML.load_file('lib/config/settings.yml')[settings_group]
     @solr_url = yml["solr_url"]
     @solrj_jar_dir = yml["solrj_jar_dir"]
     @solrj_queue_size = yml["solrj_queue_size"]
